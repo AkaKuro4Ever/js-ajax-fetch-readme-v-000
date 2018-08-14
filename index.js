40e7985e569bc5501a62e02eea29c5c3b11ac5ac
Deborah Seow <deborahseow9@gmail.com>
2018-08-13 19:05:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much.";
+
+const token = '2e99473f4f59d46e50a02f0f8f0cc6369b6d0774';
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
+  // 
+  //
+  // fetch('https://api.github.com/user/repos').
+  //   then(res => res.json()).
+  //   then(json => console.log(json))
